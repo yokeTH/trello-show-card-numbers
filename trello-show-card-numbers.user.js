9a29a876191393660ca9ac13f164c657d05a1d4a
DefeNerTH <sern.dev@gmail.com>
2021-07-16 03:19:07 +0700

    Beauty num card

--- trello-show-card-numbers.user.js
+++ trello-show-card-numbers.user.js
@@ -11,18 +11,30 @@
 
 (function() {
     'use strict';
-
+    // --- config ---
+    const enableBeautifulNum = true;
+    const fontColor = "#F8F8FF";
+    const bgColor = "#DB7093";
+    const waitLoad = 3
     // execute when page loaded
     window.addEventListener('load', () => {
         // get html element by class `card-short-id`
         const htmlElement = document.getElementsByClassName("card-short-id");
         
-        // wait 3 sec
+        // wait load
         setTimeout(()=>{
             // loop remove hide class `hide` all element in `htmlElement`
             for (let i = 0 ; i < htmlElement.length ; i+=1){
                 htmlElement[i].classList.remove("hide");
+                if (enableBeautifulNum) {
+                    htmlElement[i].style.color = fontColor;
+                    htmlElement[i].style.border = "0px solid";
+                    htmlElement[i].style.borderRadius = "10%";
+                    htmlElement[i].style.backgroundColor = bgColor;
+                    htmlElement[i].style.padding = "2px";
+                    htmlElement[i].style.marginRight = "2px";
+                }
             }
-        },3000)
+        },waitLoad * 1000)
     })
 })();
