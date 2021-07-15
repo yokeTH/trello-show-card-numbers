fd6e35f4504aa85beafd63261423c4d6e6af0a06
DefeNerTH <sern.dev@gmail.com>
2021-07-16 02:20:26 +0700

    Finish

--- trello-show-card-numbers.user.js
+++ trello-show-card-numbers.user.js
@@ -12,6 +12,17 @@
 (function() {
     'use strict';
 
-    // Your code here...
-
+    // execute when page loaded
+    window.addEventListener('load', () => {
+        // get html element by class `card-short-id`
+        const htmlElement = document.getElementsByClassName("card-short-id");
+        
+        // wait 3 sec
+        setTimeout(()=>{
+            // loop remove hide class `hide` all element in `htmlElement`
+            for (let i = 0 ; i < htmlElement.length ; i+=1){
+                htmlElement[i].classList.remove("hide");
+            }
+        },3000)
+    })
 })();
