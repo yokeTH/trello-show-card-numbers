9737317144a4e8db182e25a5d2805e3e4418d084
DefeNerTH <sern.dev@gmail.com>
2021-07-16 01:14:54 +0700

    git init

---
+++ trello-show-card-numbers.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Trello Show Card Numbers
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Show Card Number for trello.com
+// @author       yokeTH
+// @match        https://trello.com/b/7iVZq9m3/dev-tasks
+// @icon         https://www.google.com/s2/favicons?domain=trello.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+
+})();
